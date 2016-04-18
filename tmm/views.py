263668c597aa845c0f90675d36339ee96ad56471
Rob <R.Treharne@liverpool.ac.uk>
2016-04-18 16:57:53 +0100

    json return working

--- tmm/views.py
+++ tmm/views.py
@@ -4,6 +4,10 @@ import json
 
 def home(request):
     projects = Project.objects.all()
+    project = projects[0]
     digit = len(projects)
-    return render(request, 'tmm.html', {'projects': json.dumps(projects[0].json), 'digit':digit})
+    
+    project.json = json.dumps(project.json)
+
+    return render(request, 'tmm.html', {'projects': json.dumps(projects[0].json).encode('utf8'), 'project': project})
 
